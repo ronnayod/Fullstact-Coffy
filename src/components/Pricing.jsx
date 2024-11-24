9f5f70fe8eb0fd088e31e18dfac4e48831e5f672
ronnayod <gaen172543@gmail.com>
2024-11-24 16:12:23 +0700

    first commit

---
+++ src/components/Pricing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Pricing() {
+  return (
+    <div>
+      <h2>Pricing Page</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat saepe reiciendis quisquam libero at ipsam.
+      </p>
+    </div>
+  )
+}
+
+export default Pricing
---
+++ src/components/Services.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Services() {
+  return (
+    <div>
+      <h2>Services Page</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quam totam exercitationem voluptates blanditiis cupiditate nemo sint eligendi reprehenderit hic.
+      </p>
+    </div>
+  )
+}
+
+export default Services
