757bc25859bbff9de732bb4f54cb91ad62f0bf8e
dog <panwan.21st@gmail.com>
2024-11-27 21:43:21 +0700

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 // ทำการ npm i react-router-dom แล้วเรียกใช้งาน
