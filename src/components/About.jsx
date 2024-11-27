c679cf0ffd311c56a93cd1957bf086fdd1ac564a
dog <panwan.21st@gmail.com>
2024-11-27 21:20:49 +0700

    Revert "initial project (reset)"
    This reverts commit 1d17e9d11bf87bd3966da1ed2a011602e6f98af1.

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, neque.
+      </p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <hr />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel nobis illo praesentium officiis delectus harum dolorem! Doloremque.
+      </p>
+    </div>
+  )
+}
+
+export default Home
